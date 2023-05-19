cf3bc76d54f607141483d0ebc1b0fe884a6da126
DavideSberna <sbernadavide@gmail.com>
2023-05-19 17:22:07 +0200

    change root folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <section>
         <nav>
-            <ul>
+            <ul class="navbar-nav flex-row">
                 @foreach($nav as $page)
-                    <li><a href="{{ route($page) }}">{{ $page }}</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route($page) }}">{{ $page }}</a></li>
                 @endforeach
             </ul>
         </nav>
     </section>
+    <?php
+    echo $ciao?>
 </body>
 </html>
